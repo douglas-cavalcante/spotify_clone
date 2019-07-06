5346cdeb6ed306c24992440da7ad464f2de63a31
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-06 16:53:01 -0300

    Adiciona eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 
-import "./config/ReactotronConfig";
+import "~/config/ReactotronConfig";
 
 import { Provider } from "react-redux";
 import store from "./store";
