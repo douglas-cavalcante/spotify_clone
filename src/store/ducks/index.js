a584522a7c5d53538c2c75f49eaed21d91752060
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-08 17:24:34 -0300

    Configura duck de podcast

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
+import { reducer as podcats } from "./podcats";
+
 const reducers = combineReducers({
-  // Remova essa linha depois de adicionar seus ducks
-  example: () => []
+    // Remova essa linha depois de adicionar seus ducks
+    podcats
 });
 
 export default reducers;
